49312b1ad2dc4b4fe4a3b6e9af46edccb88ccea1
Mateusz Gadzina <gadzinam28@gmail.com>
2026-04-06 22:09:46 +0200

    refactor: convert DATA from dict to list

--- data.py
+++ data.py
@@ -1,12 +1,30 @@
-DATA = {
-    "games": {
-        "kid":["Minecraft"],
-        "teen": ["Fortnite"],
-        "adult": ["Elden Ring", "Battlefield V"]
+##TODO: Add tags to each block in DATA to better recommend the client games etc.
+
+DATA = [
+    {
+        "name": "Fortnite",
+        "category": "games",
+        "age_group": "teen"
     },
-    "music": {
-        "kid": ["Disney songs"],
-        "teen": ["Pop playlist"],
-        "adult": ["Skillet"]
-    }
-}
+    {
+        "name": "Elden Ring",
+        "category": "games",
+        "age_group": "adult"
+    },
+    {
+        "name": "Minecraft",
+        "category": "games",
+        "age_group": "kid"
+    },
+    {
+        "name": "Skillet",
+        "category": "music",
+        "age_group": "adult"
+    },
+    {
+        "name": "Pop playlist",
+        "category": "music",
+        "age_group": "teen"
+    },
+]
+
